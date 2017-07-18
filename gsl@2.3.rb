f78c60386010af55695de0d96152274505100135
ajm <ajm@asiaa.sinica.edu.tw>
2017-07-18 10:22:48 +0800

    renaming as @2.3

---
+++ gsl@2.3.rb
@@ -0,0 +1,18 @@
+class GslAT23 < Formula
+  desc "Numerical library for C and C++"
+  homepage "https://www.gnu.org/software/gsl/"
+  url "https://ftp.gnu.org/gnu/gsl/gsl-2.3.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gsl/gsl-2.3.tar.gz"
+  sha256 "562500b789cd599b3a4f88547a7a3280538ab2ff4939504c8b4ac4ca25feadfb"
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make" # A GNU tool which doesn't support just make install! Shameful!
+    system "make", "install"
+  end
+
+  test do
+    system bin/"gsl-randist", "0", "20", "cauchy", "30"
+  end
+end
+
