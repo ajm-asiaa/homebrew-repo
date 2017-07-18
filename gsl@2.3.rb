8a7bb2d3e99d94ff8967956bc7c82157b0df5316
ajm <ajm@asiaa.sinica.edu.tw>
2017-07-18 10:41:41 +0800

    Adding bottle sha256 for Sierra and el Capitan

--- gsl@2.3.rb
+++ gsl@2.3.rb
@@ -5,6 +5,12 @@ class GslAT23 < Formula
   mirror "https://ftpmirror.gnu.org/gsl/gsl-2.3.tar.gz"
   sha256 "562500b789cd599b3a4f88547a7a3280538ab2ff4939504c8b4ac4ca25feadfb"
 
+  bottle do
+    cellar :any
+    sha256 "40ac9d17c2ee403d5d967b8f9473f1b43f2fc78f734d02f98b1843528ac65799" => :sierra
+    sha256 "b9115718eae20160d6c1166a9ca4261e3a867d019f51907ef0674a45dee9eb9d" => :el_capitan
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make" # A GNU tool which doesn't support just make install! Shameful!
